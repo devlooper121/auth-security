f20942763bdf074d493aca2e542829a8715e6b29
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-05-11 11:33:51 +0530

    level 1 security only user id and pasword

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// setup app using express 
+const app = express();
+// set app to use bodyParser middleware
+app.use(bodyParser.urlencoded({extended:true}));
+// set app to use ejs middleware 
+app.set('view engine', 'ejs');
+
+// set app to use public folder
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+    email: {
+        type : String,
+        required: [true, "email is required"]
+    },
+    password:{
+        type: String,
+        required: [true, "password is also required"]
+    }
+}
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", (req, res)=>{
+    res.render("home")
+})
+
+app.get("/login", (req, res)=>{
+    res.render("login")
+})
+
+app.get("/register", (req, res)=>{
+    res.render("register")
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is started at port 3000");
+})
+
+app.post("/register",(req, res)=>{
+    
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(err=>{
+        if(!err){
+            res.render("secrets");
+        }else{
+            log(err);
+        }
+    })
+});
+
+app.post("/login", (req, res)=>{
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:userName}, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser!= null){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }else{
+                    res.send("wrong password");
+                }
+            }
+        }
+    })
+})
